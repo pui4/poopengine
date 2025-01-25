203397c3ff736e5762b59296c06b7d4a45c82104
Precious13ui (Daniel) <70605806+pui4@users.noreply.github.com>
2025-01-25 22:26:56 +0000

    Broken but its prob gonna be all for tonight

---
+++ example/main.js
@@ -0,0 +1,23 @@
+const originalStart = poopengine.start;
+poopengine.start = function () {
+  originalStart.apply(this);
+  const box = poopengine.object({
+    width: 50,
+    height: 50,
+    x: 0,
+    y: 0,
+    colour: 'blue'
+  });
+}
+
+const originalUpdate = poopengine.update;
+poopengine.update = function () {
+  originalUpdate.apply(this);
+  // Code goes here:
+}
+
+poopengine.resize = function () {
+  // Code goes here:
+}
+
+poopengine.start();
---
+++ example/not_main.js
@@ -0,0 +1,25 @@
+import { poopengine } from '../poopengine.mjs';
+
+const originalStart = poopengine.start;
+poopengine.start = function () {
+  originalStart.apply(this);
+  const box = poopengine.object({
+    width: 50,
+    height: 50,
+    x: 0,
+    y: 0,
+    colour: 'red'
+  });
+}
+
+const originalUpdate = poopengine.update;
+poopengine.update = function () {
+  originalUpdate.apply(this);
+  // Code goes here:
+}
+
+poopengine.resize = function () {
+  // Code goes here:
+}
+
+poopengine.start();
